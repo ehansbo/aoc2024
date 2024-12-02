5f5fb95b649220df97e35a269da8f7302221e181
Erik Hansbo <erik.hansbo@seb.se>
2024-12-02 10:48:49 +0100

    Change to foldl1

--- DayTwo.hs
+++ DayTwo.hs
@@ -12,7 +12,7 @@ solve1 xs = length $ filter valid xs
 solve2 :: [[Int]] -> Int
 solve2 (xs:xss) = 
     let alternatives = xs : map (\i -> fst (splitAt i xs) ++ tail (snd (splitAt i xs))) [0..length xs-1]
-    in if foldl (||) False (map valid alternatives) then 1 + solve2 xss else 0 + solve2 xss
+    in if foldl1 (||) (map valid alternatives) then 1 + solve2 xss else 0 + solve2 xss
 solve2 _ = 0
 
 valid :: [Int] -> Bool
@@ -20,5 +20,5 @@ valid xs =
     let getDiffs (y:z:ys) = z-y : getDiffs (z:ys)
         getDiffs _ = []
         diffs = getDiffs xs
-        cmpDiffs f = foldl (&&) True (map f diffs)
+        cmpDiffs f = foldl1 (&&) (map f diffs)
     in (cmpDiffs (<0) || cmpDiffs (>0)) && cmpDiffs ((<4) . abs)
